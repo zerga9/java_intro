3331b2b11b933af3cfe6fd16916e25131c0d34db
Gadiza Zerari <gadizaz@hotmail.com>
2018-06-15 12:59:35 +0100

    Numbers exercise done

--- chapter2-DataTypes/exercises/Numbers.java
+++ chapter2-DataTypes/exercises/Numbers.java
@@ -1,12 +1,16 @@
 public class Numbers {
   public static void main(String[] args) {
     // declare an integer variable with a value
+    int number = 50;
 
     // declare a double variable with a value
+    double doubleNumber = 8.7;
 
     // store the result of adding the two together in a variable
+    double sum = number + doubleNumber;
 
     // print it
+    System.out.println(sum);
 
   }
 }
