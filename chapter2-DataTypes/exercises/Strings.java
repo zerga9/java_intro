b1c72d9b965f78e0e976c276ca66799da244ef7b
Gadiza Zerari <gadizaz@hotmail.com>
2018-06-15 12:51:18 +0100

    Strings exercise chapter 2

--- chapter2-DataTypes/exercises/Strings.java
+++ chapter2-DataTypes/exercises/Strings.java
@@ -1,12 +1,15 @@
 public class Strings {
   public static void main(String[] args) {
     // declare the first name
-
+    String myName = "Gadiza";
     // declare the second name
+    String hisName = "Neil";
 
     // create your message
-
+    String output = String.format("%s and %s are learning Java", myName, hisName);
+    
     // print your message!
+    System.out.println(output);
 
   }
 }
