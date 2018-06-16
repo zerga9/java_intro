6236b85aa4165d97d631b2e4de83444f2375f757
Gadiza Zerari <gadizaz@hotmail.com>
2018-06-16 07:56:24 +0100

    Chapter 3 control flow exercise done

--- chapter3-JavaBuzz/exercises/ControlFlow.java
+++ chapter3-JavaBuzz/exercises/ControlFlow.java
@@ -1,10 +1,24 @@
 // what imports will you need?
-
+import java.util.Scanner;
 // can you remember how to set up your main function?
+public class ControlFlow {
+public static void main(String args[]) {
+  System.out.println("Enter a number here: ");
+  // find a way to read in user input
+  Scanner input = new Scanner(System.in);
+  // write a method that will check if a number is odd or even
+  // (assume user only ever enters integers)
+  int number = input.nextInt();
 
-// find a way to read in user input
+  String result;
 
-// write a method that will check if a number is odd or even
-// (assume user only ever enters integers)
-
+  if(number%2 == 0) {
+    result = "even number";
+  } else {
+    result = "odd number";
+  }
 // print the answer to the console
+  String message = String.format("You've entered a %s.", result);
+  System.out.println(message);
+  }
+}
